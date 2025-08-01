cb40464bf0ffa3d082e12579b3f314a56b6dac5e
panda <panda@192.168.100.16>
2025-08-01 15:58:32 -0300

    API its running with this route: http://localhost:5263/api/tasks

--- Controllers/ControllerTasks.cs
+++ Controllers/ControllerTasks.cs
@@ -1,4 +1,22 @@
 using Microsoft.AspNetCore.Mvc;
-using TaskAPI.Model;
+using TaskAPI.Models;
 
 namespace TaskAPI.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class TasksController : ControllerBase
+    {
+        private static List<TaskItem> tasks = new List<TaskItem>
+        {
+            new TaskItem { Id = 1, Title = "Estudar C#", Description = "Aprender fundamentos do C#", Complete = false},
+            new TaskItem { Id = 2, Title = "Estudar PostgreSQL", Description = "Criar tabelas e fazer consultas", Complete = false}
+        };
+
+        [HttpGet]
+        public ActionResult<List<TaskItem>> GetTasks()
+        {
+            return Ok(tasks);
+        }
+    }
+}
--- Models/TaskItem.cs
+++ Models/TaskItem.cs
@@ -1,10 +1,10 @@
 namespace TaskAPI.Models
 {
-    public class Task
+    public class TaskItem
     {
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+        public string? Title { get; set; }
+        public string? Description { get; set; }
         public bool Complete { get; set; }
     }
 }
