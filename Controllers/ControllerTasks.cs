9f6590b972f3f0bb56f2c1bcfc1fb06b7c6073f5
panda <panda@192.168.100.16>
2025-08-01 15:07:53 -0300

    Initial of Task.cs and touch ControllerTasks.cs

---
+++ Controllers/ControllerTasks.cs
@@ -0,0 +1,4 @@
+using Microsoft.AspNetCore.Mvc;
+using TaskAPI.Model;
+
+namespace TaskAPI.Controllers
---
+++ Models/Task.cs
@@ -0,0 +1,10 @@
+namespace TaskAPI.Models
+{
+    public class Task
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public bool Complete { get; set; }
+    }
+}
